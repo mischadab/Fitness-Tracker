21d97876d7855b27ec53d569834efafdca7da80f
mischadab <mischadabrowski@gmail.com>
2021-03-11 16:54:38 -0500

    added server

---
+++ server.js
@@ -0,0 +1,30 @@
+// requirements
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// connect to routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+
+// start the app listening
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
