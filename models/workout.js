3a23cffcf2d6cb28f235ac5ed1e7464848c08b14
mischadab <mischadabrowski@gmail.com>
2021-03-11 15:04:01 -0500

    added schema to models

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'Enter an exercise type'
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: 'Enter the name of your exercise'
+                },
+                duration: {
+                    type: Number,
+                    trim: true,
+                    required: 'Enter the duration of your exercise'
+                },
+                weight: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                }
+            }
+        ]
+    }
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+
+);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
