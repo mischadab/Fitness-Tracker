8f51de0b56a85cca27dff4e97d384587f259c149
mischadab <mischadabrowski@gmail.com>
2021-03-11 15:17:37 -0500

    added router post to api route

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+// require built in router in express.js
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+
+// api route to post data to the database
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(workoutDB => {
+        res.json(workoutDB)
+    }) 
+});
