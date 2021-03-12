a672794a09abe5668703ee608056d933e62a5ffa
mischadab <mischadabrowski@gmail.com>
2021-03-12 13:39:25 -0500

    removed delete because it doesnt do anything

--- routes/api.js
+++ routes/api.js
@@ -69,15 +69,5 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     });
 });
 
-// delete a workout
-router.delete("/api/workouts", ({ body}, res) => {
-    Workout.findByIdAndDelete(body, id)
-    .then(() => {
-        res.json(true);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
-});
 
 module.exports = router;
